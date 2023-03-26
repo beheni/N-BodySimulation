a8292547cbc884d3c40512187e5bd88779ab8941
Sofiia Folvarochna <sofiyafolv@gmail.com>
2023-03-26 22:17:21 +0300

    added Texture maybe it works

--- NBodySimulation/NBodySim/include/Texture.h
+++ NBodySimulation/NBodySim/include/Texture.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
-/*
-#include <stb_image.h>
+#include <stb_image/stb_image.h>
+
 
 class Texture {
 public:
@@ -9,50 +9,13 @@ public:
     int width;
     int height;
 
-    Texture(int w, int h) : width(w), height(h) {
-        glGenTextures(1, &id);
-        glBindTexture(GL_TEXTURE_2D, id);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
+    Texture(int w, int h);
 
-    Texture(const char* filepath) {
-        stbi_set_flip_vertically_on_load(true);
+    Texture(const char* filepath);
 
-        unsigned char* data = stbi_load(filepath, &width, &height, nullptr, 0);
-        if (!data) {
-            std::cerr << "Failed to load texture: " << filepath << std::endl;
-            return;
-        }
+    ~Texture();
 
-        glGenTextures(1, &id);
-        glBindTexture(GL_TEXTURE_2D, id);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glBindTexture(GL_TEXTURE_2D, 0);
+    void bind(unsigned int slot = 0) const;
 
-        stbi_image_free(data);
-    }
-
-    ~Texture() {
-        glDeleteTextures(1, &id);
-    }
-
-    void bind(unsigned int slot = 0) const {
-        glActiveTexture(GL_TEXTURE0 + slot);
-        glBindTexture(GL_TEXTURE_2D, id);
-    }
-
-    void unbind() const {
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
+    void unbind() const;
 };
-
-*/
--- NBodySimulation/NBodySim/src/Texture.cpp
+++ NBodySimulation/NBodySim/src/Texture.cpp
@@ -1,1 +1,51 @@
 #include "Texture.h"
+
+
+Texture::Texture(int w, int h) : width(w), height(h) {
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+Texture::Texture(const char* filepath) {
+    stbi_set_flip_vertically_on_load(true);
+
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    unsigned char* data = stbi_load(filepath, &width, &height, nullptr, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cerr << "Failed to load texture: " << filepath << std::endl;
+        return;
+    }        
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    stbi_image_free(data);
+}
+
+Texture::~Texture() {
+    glDeleteTextures(1, &id);
+}
+
+void Texture::bind(unsigned int slot) const {
+    glActiveTexture(GL_TEXTURE0 + slot);
+    glBindTexture(GL_TEXTURE_2D, id);
+}
+
+void Texture::unbind() const {
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
