d82811541b7b3e924310cd87de52e40e0fdfd4d7
Sofiia Folvarochna <sofiyafolv@gmail.com>
2023-03-27 09:00:00 +0300

    minor

--- NBodySimulation/NBodySim/src/Texture.cpp
+++ NBodySimulation/NBodySim/src/Texture.cpp
@@ -19,7 +19,7 @@ Texture::Texture(const char* filepath) {
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     unsigned char* data = stbi_load(filepath, &width, &height, &nrChannels, 0);
