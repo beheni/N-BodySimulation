4a931a7778a8fa9ed875f0457593a356fad2fb5c
beheni <anastasiia.beheni@ucu.edu.ua>
2023-04-01 17:05:01 +0300

    шоб в роми кров з очей не йшла

--- NBodySimulation/NBodySim/src/Texture.cpp
+++ NBodySimulation/NBodySim/src/Texture.cpp
@@ -1,7 +1,10 @@
 #include "Texture.h"
 
 
-Texture::Texture(int w, int h) : width(w), height(h) {
+Texture::Texture(int w, int h) 
+    : 
+    width(w), height(h) 
+{
     glGenTextures(1, &id);
     glBindTexture(GL_TEXTURE_2D, id);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -13,7 +16,8 @@ Texture::Texture(int w, int h) : width(w), height(h) {
     unbind();
 }
 
-Texture::Texture(const char* filepath) {
+Texture::Texture(const char* filepath) 
+{
     stbi_set_flip_vertically_on_load(true);
     glGenTextures(1, &id);
     glBindTexture(GL_TEXTURE_2D, id);
@@ -23,26 +27,31 @@ Texture::Texture(const char* filepath) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     unsigned char* data = stbi_load(filepath, &width, &height, &nrChannels, 0);
-    if (data) {
+    if (data) 
+    {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else {
+    else 
+    {
         throw std::runtime_error("Failed to load texture: " + std::string(filepath));
         return;
     }        
     stbi_image_free(data);
 }
 
-Texture::~Texture() {
+Texture::~Texture() 
+{
     glDeleteTextures(1, &id);
 }
 
-void Texture::bind(unsigned int slot) const {
+void Texture::bind(unsigned int slot) const 
+{
     glActiveTexture(GL_TEXTURE0 + slot);
     glBindTexture(GL_TEXTURE_2D, id);
 }
 
-void Texture::unbind() const {
+void Texture::unbind() const 
+{
     glBindTexture(GL_TEXTURE_2D, 0);
 }
